bdf02cb643536cf562117c4eb4365d74dd5facd3
Alexis Trousset <alexis.trousset@yahoo.fr>
2020-01-20 15:34:14 +0100

    :construction: Set routes

--- back/api/routes/rooter.js
+++ back/api/routes/rooter.js
@@ -2,6 +2,15 @@
 
 const express = require('express');
 const router = express.Router();
+const auth = require('../helpers/security/authenticator');
 
 //Import different controllers
+const securityController = require('../controllers/security');
 
+
+/**
+ * Route definition
+ **/
+
+router.post('/sign-in', securityController.sign_in);
+router.post('/sign-up', securityController.sign_up);

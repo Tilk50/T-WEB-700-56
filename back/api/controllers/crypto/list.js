dcdc3d03379a5a0a1231c53b3c98b247b64ac9a9
Alexis Trousset <alexis.trousset@yahoo.fr>
2020-01-29 20:54:05 +0100

    :sparkles: Create server side request

--- back/api/controllers/crypto/list.js
+++ back/api/controllers/crypto/list.js
@@ -6,10 +6,41 @@ const Crypto = new mongoose.model('crypto', cryptoSchema);
 const boom = require('@hapi/boom');
 
 const list = function (req, res, next) {
-  Crypto.find(function (err, cryptoList) {
-     if (err) return next(boom.internal("no data aviable"));
-     res.json({status: "success", list: cryptoList});
-  });
+    let count = 0;
+    let filter = buildFilter(JSON.parse(req.query.filter));
+    // This query is used to get the
+    Crypto.find(
+        filter
+    ).exec(function (err, result) {
+        if (err) return next(boom.internal("no data aviable"));
+       count = result.length;
+    });
+
+    // This query is used to return data
+    Crypto
+      .find(
+          filter
+      )
+      .limit(parseInt(req.query.rowsRequest))
+      .sort([[req.query.sortBy, parseInt(req.query.desc)]])
+      .skip(parseInt(req.query.startRow))
+      .exec(function(err, result) {
+         if (err) return next(boom.internal("no data aviable"));
+         // Return data
+          res.json({list: result, totalRow: count});
+      });
 };
+
+function buildFilter(queryFilter) {
+    let filter = {};
+    if(typeof queryFilter.name !== 'undefined' && queryFilter.name !== '') {
+      filter.name = new RegExp(queryFilter.name, 'i');
+    }
+
+    if (typeof queryFilter.symbol !== 'undefined' && queryFilter.symbol !== '') {
+      filter.symbol = new RegExp(queryFilter.symbol, 'i');
+    }
+    return filter
+}
 
 module.exports = list;

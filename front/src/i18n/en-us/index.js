0c06e72c0072bd26e11eb5fc69e4570eda08b722
touria oudadsse <toudadsse@gmail.com>
2020-02-17 11:39:34 +0100

    :lipstick: [FRONT]
    Update translate

--- front/src/i18n/en-us/index.js
+++ front/src/i18n/en-us/index.js
@@ -1,10 +1,8 @@
-// This is just an example,
-// so you can safely delete all default props below
 
 export default {
   global_page: {
     title: 'The court of money',
-    choose_language: 'Choose a language',
+    choose_language: 'Search cryptocurrency',
     languages: {
       french: 'French',
       english: 'English'
@@ -13,44 +11,89 @@ export default {
       tool_list_title: 'Tools list',
       tools: {
         search: 'Search',
-        search_title: 'Search crypto - money',
-        fav_title: 'My favs'
+        search_title: 'Rechercher une crypto monnaie',
+        fav_title: 'My favorites'
       }
     },
     label: {
-      crypto_monnaie: 'Crypto - money',
-      account: 'My account'
+      crypto_monnaie: 'cryptocurrency',
+      account: 'My account',
+      admin_panel: 'Control panel',
+      load_data: 'Refresh the data',
+      users: 'Users',
+      actions: 'Action',
+      warning: 'Warning !'
+    },
+    confirm_dialog: {
+      delete_user: 'Are you sure you want to delete this user?'
     }
   },
   errors: {
     title: 'Warning',
     no_search: 'You have not entered any search criteria',
-    enter_mail: 'Vous devez entrer votre addresse mail au bon format',
-    password_different: 'Both password must be equals',
-    mail_already_use: 'This mail address is already used'
+    enter_mail: 'You must enter your email address in the correct format',
+    password_different: 'Both passwords must be identical',
+    mail_already_use: 'This email address is already registered',
+    unauthorized: 'Warning, you are not allowed to access this feature',
+    invalid_token: 'Your session has expired, please relogin',
+    invalid_credential: 'Wrong login or password',
+    unidentified: 'You are not connected'
   },
   labels: {
+    update_my_info: 'Updating my information',
+    edit_my_infos: 'Modify my information',
+    actions: 'Action',
     close: 'Close',
-    see: 'See',
-    log_in: 'Login in',
-    login: 'Connexion',
-    log_out: 'Log out',
+    see: 'View',
+    log_in: 'Log_in',
+    login: 'Login',
+    log_out: 'log out',
     mail_address: 'Email address',
     password: 'Password',
     cancel: 'Cancel',
-    sign_in: 'S\'inscire',
-    password_length: 'Longueur du mot de passe superieure à 8 caractères',
-    password_uppercase: 'Le mot de passe contient au moins une majuscule',
-    password_number: 'Le mot de passe contient au moins un chiffre',
-    password_special: 'Le mot de passe contient au moins un caractère spécial',
-    validate: 'Valider'
+    sign_in: 'Sign in',
+    password_length: 'Password must be at least 8 characters in length',
+    password_uppercase: 'The password contains at least one uppercase, or capital, letter ',
+    password_number: ' The password contains at least one number digit',
+    password_special: 'The password contains at least one special character',
+    validate: 'Validate',
+    crypto_list: 'List of cryptocurrency ',
+    user_list: 'List of users',
+    price_evolution: 'Price evolution (in €)',
+    hide_diagrams: 'Hide the diagrams',
+    show_diagrams: 'Show the diagrams',
+    price_chart: 'Evolution of the court of this cryptocurrency',
+    crypto_object: {
+      name: 'Name',
+      symbol: 'Symbol',
+      actual_price: 'Current price (in €)',
+      show_in_list: 'Show in the list',
+      percent_change_1H: 'Change during the hour (%)',
+      percent_change_24H: 'Change during the day (%)',
+      percent_change_7D: 'Change during the week (%)',
+      market_cap: 'Market cap'
+    },
+    add_fav: 'Add  to favorites',
+    remove_fav: 'Remove from favorites',
+    my_account: 'My account',
+    my_account_information: 'General information of my account',
+    user_object: {
+      email: 'Email',
+      is_admin: 'Is admin'
+    },
+    no_favs: 'You have no favorites',
+    account_created: 'Your account has been created',
+    crypto_added_to_favs: 'Crypto well added to your favorites',
+    crypto_removed_from_favs: 'Crypto removed from favorites',
+    data_uploaded: 'Data has been updated',
+    account_identity: 'Identity linked to the account'
   },
   hints: {
-    enter_mail_address: 'Entrez votre adresse mail',
-    enter_password: 'Entrez votre mot de passe',
-    password_confirm: 'Confirmez votre mot de passe'
+    enter_mail_address: 'Enter your email address',
+    enter_password: 'Enter your password',
+    password_confirm: 'confirm your password'
   },
   messages: {
-    have_to_login_to_fav: 'Vous devez être connectés pour avoir une liste de favoris'
+    have_to_login_to_fav: 'You must be logged-in to have a favorites list'
   }
 }
